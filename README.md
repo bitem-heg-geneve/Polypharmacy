c1b7e5efba453b02e7eb1306c4d9efd58e51eaf8
mottinl <luc.mottin@hesge.ch>
2024-06-05 19:02:31 +0200

    Code editing for re-use Minimal README for DEV

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Polypharmacy
 Repository for Polypharmacy - project with Bayartis
+
+DEV deployment in a virtualenv:
+
+1) git clone https://github.com/bitem-heg-geneve/Polypharmacy.git
+2) cd Polypharmacy
+3) python -m venv venv
+4) .\.venv\Scripts\activate
+5) pip install -r .\src\requirements.txt
+6) fastapi dev sources\app.py
--- src/app.py
+++ src/app.py
@@ -1,9 +1,3 @@
-# installation (in a virtualenv):
-# pip install fastapi uvicorn[standard] requests
-# 
-# execution:
-# fastapi dev sources\app.py
-
 import requests
 from typing import List, Dict
 from fastapi import FastAPI, Query, Request
@@ -12,6 +6,7 @@ from enum import Enum
 from collections import defaultdict
 
 
+# Initialize FastAPI app
 app = FastAPI(
     title="My API",
     description="""Long description of the API, on multiple lines""",
@@ -21,40 +16,43 @@ app = FastAPI(
     openapi_url="/openapi.json",
 )
 
-
-# available languages
+# Define Enum for "Accept-Language" header in the query to compendium
 class Language(Enum):
     FR = "fr-CH"
     DE = "de-CH"
 
-
+# Define model for drug interaction
 class Interaction(BaseModel):
     id: str
     name: str
     mechanism: str
 
-
+# Define model for drug
 class Drug(BaseModel):
     gtin: str
     #name: str
     description: str | None = None
     interactions: list[Interaction]
 
-
+# Store drugs by GTIN
 STORE: dict[Drug] = {}
 
 
+# Endpoint to get interactions for multiple GTINs
 @app.get("/interactions_multiple_gtins", response_model=List[Interaction])
 def get_interactions_multiple_gtins(
     gtins: str = Query(description="drug GTINs, separated by commas", example="7680612850014,7680531140760"),
     language: Language = Query(default=Language.FR)
 ) -> List[Drug]:
+    # Split GTINs into list
     gtin_list = gtins.split(',')
     all_drugs = []
 
+    # Count interactions and create interaction map
     interaction_count: Dict[str, int] = defaultdict(int)
     interaction_map: Dict[str, Interaction] = {}
 
+    # Fetch data from compendium for each GTIN
     for gtin in gtin_list:
         response = requests.get(
             f"https://documedis.hcisolutions.ch/2020-01/api/products/{gtin}?IdType=gtin",
@@ -63,31 +61,37 @@ def get_interactions_multiple_gtins(
         response.raise_for_status()
         data = response.json()
 
+        # Extract drug interactions from response
         interactions = [
             get_interaction(drugInteraction)
             for drugInteraction in data["components"][0]["substances"][0]["drugInteractions"]
         ]
 
+        # Update interaction count and map
         for interaction in interactions:
             interaction_count[interaction.id] += 1
             interaction_map[interaction.id] = interaction
 
+    # Filter matching interactions
     repeated_interactions = [
         interaction for interaction_id, count in interaction_count.items() if count > 1
         for interaction in [interaction_map[interaction_id]]
     ]
 
+    # If no repeated interactions found, return a default interaction
     if not repeated_interactions:
         return [Interaction(id="0", name="No interaction found", mechanism="")]
 
     return repeated_interactions
 
 
+# Endpoint to get data for a single GTIN
 @app.get("/data_single_gtin", response_model=Drug)
 def get_data_single_gtin(
     gtin: str = Query(description="drug GTIN", example="7680612850014"),
     language: Language = Query(default=Language.FR)
 ) -> Drug:
+    # Fetch data for the given GTIN
     response = requests.get(
         f"https://documedis.hcisolutions.ch/2020-01/api/products/{gtin}?IdType=gtin",
         headers={"Accept-Language" : language.value}
@@ -95,11 +99,13 @@ def get_data_single_gtin(
     response.raise_for_status()
     data = response.json()
 
+    # Extract drug interactions from response
     interactions = [
         get_interaction(drugInteraction)
         for drugInteraction in data["components"][0]["substances"][0]["drugInteractions"]
     ]
 
+    # Create drug object to be returned
     drug = Drug(
         gtin=gtin,
         #name=data["name"],
@@ -110,5 +116,6 @@ def get_data_single_gtin(
     return drug
 
 
+# Function to create Interaction object
 def get_interaction(drugInteraction) -> Interaction:
     return Interaction(id=drugInteraction["id"], name=drugInteraction["title"], mechanism=drugInteraction["mechanismText"]) 
